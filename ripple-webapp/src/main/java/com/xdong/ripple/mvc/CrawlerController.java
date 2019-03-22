233d1590af9694c93e357b918a02f034bb33633d
wanglei <wanglei@wdai.com>
2019-03-22 09:40:02 +0800

    crawler_music

--- ripple-webapp/src/main/java/com/xdong/ripple/mvc/CrawlerController.java
+++ ripple-webapp/src/main/java/com/xdong/ripple/mvc/CrawlerController.java
@@ -1,16 +1,29 @@
 package com.xdong.ripple.mvc;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.xdong.ripple.dal.entity.crawler.RpCrawlerSongsDo;
 import com.xdong.ripple.spi.crawler.IRpCrawlerSongsService;
 
 @Controller
 @RequestMapping("/crawler")
-public class CrawlerController {
+public class CrawlerController extends BaseController {
+
+    private static final Logger    logger = LogManager.getLogger(CrawlerController.class);
+
+    private static final String    prefix = "crawler";
 
     @Autowired
     private IRpCrawlerSongsService rpCrawlerSongsServiceImpl;
@@ -19,5 +32,29 @@ public class CrawlerController {
     @ResponseBody
     public RpCrawlerSongsDo getSongsList(Long id) {
         return rpCrawlerSongsServiceImpl.selectById(id);
+    }
+
+    @RequestMapping(value = "/home")
+    @ResponseBody
+    public ModelAndView pageInit(int pageNo, int pageSize) {
+
+        Page<RpCrawlerSongsDo> page = new Page<RpCrawlerSongsDo>();
+        page.setCurrent(pageNo);
+        page.setSize(pageSize);
+        Page<RpCrawlerSongsDo> result = rpCrawlerSongsServiceImpl.selectPage(page);
+
+        Map<String, List<RpCrawlerSongsDo>> modelMap = new HashMap<String, List<RpCrawlerSongsDo>>();
+        modelMap.put("songs", result.getRecords());
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName(getUrl(prefix, "crawler_music"));
+        mav.addAllObjects(modelMap);
+        return mav;
+    }
+
+    @RequestMapping(value = "/songData")
+    @ResponseBody
+    public Object songData(@RequestParam("songId") String songId) {
+        return rpCrawlerSongsServiceImpl.selectById(Long.parseLong(songId));
     }
 }
