37751579b165aeb39d702b52721b11888e446f4c
wanglei <wanglei@wdai.com>
2019-05-12 23:52:21 +0800

    update

--- ripple-webapp/src/main/java/com/xdong/ripple/config/DatabaseConfiguration.java
+++ ripple-webapp/src/main/java/com/xdong/ripple/config/DatabaseConfiguration.java
@@ -25,96 +25,97 @@ import org.springframework.util.StringUtils;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.xdong.ripple.commonservice.util.SecurityUtil;
 
 @Configuration
 public class DatabaseConfiguration implements EnvironmentAware {
 
-	private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(DatabaseConfiguration.class);
+    private static final Logger     LOGGER = org.slf4j.LoggerFactory.getLogger(DatabaseConfiguration.class);
 
-	private Environment environment;
-	private RelaxedPropertyResolver datasourcePropertyResolver;
+    private Environment             environment;
+    private RelaxedPropertyResolver datasourcePropertyResolver;
 
-	@Override
-	public void setEnvironment(Environment environment) {
-		this.environment = environment;
-		this.datasourcePropertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
-	}
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+        this.datasourcePropertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
+    }
 
-	//@Bean(initMethod = "init", destroyMethod = "close")
-	public DataSource getShardingDataSource() throws SQLException {
+    // @Bean(initMethod = "init", destroyMethod = "close")
+    public DataSource getShardingDataSource() throws SQLException {
 
-		ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
-		
-		shardingRuleConfig.getTableRuleConfigs().add(getOrderTableRuleConfiguration());
-		shardingRuleConfig.getTableRuleConfigs().add(getOrderItemTableRuleConfiguration());
-		shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
-		shardingRuleConfig.getBroadcastTables().add("t_config");
-//		shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(
-//				new InlineShardingStrategyConfiguration("user_id", "ds${user_id % 2}"));
-//		shardingRuleConfig.setDefaultTableShardingStrategyConfig(
-//				new StandardShardingStrategyConfiguration("order_id", new ModuloShardingTableAlgorithm()));
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
 
-		Properties props = new Properties();
-		return ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig, props);
-	}
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderTableRuleConfiguration());
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderItemTableRuleConfiguration());
+        shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
+        shardingRuleConfig.getBroadcastTables().add("t_config");
+        // shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(
+        // new InlineShardingStrategyConfiguration("user_id", "ds${user_id % 2}"));
+        // shardingRuleConfig.setDefaultTableShardingStrategyConfig(
+        // new StandardShardingStrategyConfiguration("order_id", new ModuloShardingTableAlgorithm()));
 
-	private static KeyGeneratorConfiguration getKeyGeneratorConfiguration() {
-		KeyGeneratorConfiguration result = new KeyGeneratorConfiguration("", "order_id");
-		return result;
-	}
+        Properties props = new Properties();
+        return ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig, props);
+    }
 
-	TableRuleConfiguration getOrderTableRuleConfiguration() {
-		TableRuleConfiguration result = new TableRuleConfiguration("t_order", "ds0.t_order${0..1}");
-		result.setKeyGeneratorConfig(getKeyGeneratorConfiguration());
-		return result;
-	}
+    private static KeyGeneratorConfiguration getKeyGeneratorConfiguration() {
+        KeyGeneratorConfiguration result = new KeyGeneratorConfiguration("", "order_id");
+        return result;
+    }
 
-	TableRuleConfiguration getOrderItemTableRuleConfiguration() {
-		TableRuleConfiguration result = new TableRuleConfiguration("t_order_item", "ds0.t_order_item${0..1}");
-		return result;
-	}
+    TableRuleConfiguration getOrderTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration("t_order", "ds0.t_order${0..1}");
+        result.setKeyGeneratorConfig(getKeyGeneratorConfiguration());
+        return result;
+    }
 
-	Map<String, DataSource> createDataSourceMap() throws SQLException {
-		Map<String, DataSource> result = new HashMap<>();
-		result.put("ds0", createDataSource());
-		return result;
-	}
+    TableRuleConfiguration getOrderItemTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration("t_order_item", "ds0.t_order_item${0..1}");
+        return result;
+    }
 
-	@Bean(initMethod = "init", destroyMethod = "close")
-	public DataSource createDataSource() throws SQLException {
-		if (StringUtils.isEmpty(datasourcePropertyResolver.getProperty("url"))) {
+    Map<String, DataSource> createDataSourceMap() throws SQLException {
+        Map<String, DataSource> result = new HashMap<>();
+        result.put("ds0", createDataSource());
+        return result;
+    }
 
-			LOGGER.debug("Your database connection pool configuration is incorrect!"
-					+ " Please check your Spring profile, current profiles are:"
-					+ Arrays.toString(environment.getActiveProfiles()));
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public DataSource createDataSource() throws SQLException {
+        if (StringUtils.isEmpty(datasourcePropertyResolver.getProperty("url"))) {
 
-			throw new ApplicationContextException("Database connection pool is not configured correctly");
-		}
-		DruidDataSource druidDataSource = new DruidDataSource();
-		druidDataSource.setUrl(datasourcePropertyResolver.getProperty("url"));
-		druidDataSource.setUsername(datasourcePropertyResolver.getProperty("username"));
-		druidDataSource.setPassword(datasourcePropertyResolver.getProperty("password"));
-		druidDataSource.setInitialSize(Integer.parseInt(datasourcePropertyResolver.getProperty("initialSize")));
-		druidDataSource.setMinIdle(Integer.parseInt(datasourcePropertyResolver.getProperty("minIdle")));
-		druidDataSource.setMaxActive(Integer.parseInt(datasourcePropertyResolver.getProperty("maxActive")));
-		druidDataSource.setMaxWait(Integer.parseInt(datasourcePropertyResolver.getProperty("maxWait")));
-		druidDataSource.setTimeBetweenEvictionRunsMillis(
-				Integer.parseInt(datasourcePropertyResolver.getProperty("timeBetweenEvictionRunsMillis")));
-		druidDataSource.setMinEvictableIdleTimeMillis(
-				Integer.parseInt(datasourcePropertyResolver.getProperty("minEvictableIdleTimeMillis")));
-		druidDataSource.setValidationQuery(datasourcePropertyResolver.getProperty("validationQuery"));
-		druidDataSource.setTestWhileIdle(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testWhileIdle")));
-		druidDataSource.setTestOnBorrow(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testOnBorrow")));
-		druidDataSource.setTestOnReturn(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testOnReturn")));
-		druidDataSource.setPoolPreparedStatements(
-				Boolean.parseBoolean(datasourcePropertyResolver.getProperty("poolPreparedStatements")));
-		druidDataSource.setMaxOpenPreparedStatements(
-				Integer.parseInt(datasourcePropertyResolver.getProperty("maxOpenPreparedStatements")));
-		return druidDataSource;
-	}
+            LOGGER.debug("Your database connection pool configuration is incorrect!"
+                         + " Please check your Spring profile, current profiles are:"
+                         + Arrays.toString(environment.getActiveProfiles()));
 
-	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		return new PaginationInterceptor();
-	}
+            throw new ApplicationContextException("Database connection pool is not configured correctly");
+        }
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(datasourcePropertyResolver.getProperty("url"));
+        druidDataSource.setUsername(datasourcePropertyResolver.getProperty("username"));
+        try {
+            druidDataSource.setPassword(SecurityUtil.decryption(datasourcePropertyResolver.getProperty("password")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        druidDataSource.setInitialSize(Integer.parseInt(datasourcePropertyResolver.getProperty("initialSize")));
+        druidDataSource.setMinIdle(Integer.parseInt(datasourcePropertyResolver.getProperty("minIdle")));
+        druidDataSource.setMaxActive(Integer.parseInt(datasourcePropertyResolver.getProperty("maxActive")));
+        druidDataSource.setMaxWait(Integer.parseInt(datasourcePropertyResolver.getProperty("maxWait")));
+        druidDataSource.setTimeBetweenEvictionRunsMillis(Integer.parseInt(datasourcePropertyResolver.getProperty("timeBetweenEvictionRunsMillis")));
+        druidDataSource.setMinEvictableIdleTimeMillis(Integer.parseInt(datasourcePropertyResolver.getProperty("minEvictableIdleTimeMillis")));
+        druidDataSource.setValidationQuery(datasourcePropertyResolver.getProperty("validationQuery"));
+        druidDataSource.setTestWhileIdle(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testWhileIdle")));
+        druidDataSource.setTestOnBorrow(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testOnBorrow")));
+        druidDataSource.setTestOnReturn(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("testOnReturn")));
+        druidDataSource.setPoolPreparedStatements(Boolean.parseBoolean(datasourcePropertyResolver.getProperty("poolPreparedStatements")));
+        druidDataSource.setMaxOpenPreparedStatements(Integer.parseInt(datasourcePropertyResolver.getProperty("maxOpenPreparedStatements")));
+        return druidDataSource;
+    }
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 }
